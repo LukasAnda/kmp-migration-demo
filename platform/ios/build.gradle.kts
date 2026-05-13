9bad0975c65a915bea8f4e46dc6d51651753b6f0
Lukáš Anda <lukas.anda@gmail.com>
2026-05-13 13:09:30 +0200

    Upgrade to AGP 9.1, Gradle 9.4, Kotlin 2.1.20

--- platform/ios/build.gradle.kts
+++ platform/ios/build.gradle.kts
@@ -19,10 +19,8 @@ android {
 
 kotlin {
     androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 
